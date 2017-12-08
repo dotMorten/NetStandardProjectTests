2c545ccd4b4468ad027fc56319f0fd45df692117
Morten Nielsen <no@spam.thx>
2017-12-08 00:22:07 -0800

    Implemented iOS

--- src/NetStandardTests.UI.iOS/MyControl.cs
+++ src/NetStandardTests.UI.iOS/MyControl.cs
@@ -7,22 +7,29 @@ using CoreGraphics;
 
 namespace NetStandardTests.UI
 {
+    [Foundation.Register("MyControl")]
+    [System.ComponentModel.DisplayName("MyControl")]
     public class MyControl : UIView
     {
         private SharedClass sharedClass;
+        private UILabel text;
+
         public MyControl()
         {
+            text = new UILabel();
+            AddSubview(text);
         }
-
-
+        
         public SharedClass MyProperty
         {
             get { return sharedClass; }
             set
             {
                 sharedClass = value;
-                // tv.Text = value?.ToString() ?? "";
-                // tv.SetTextColor(new Android.Graphics.Color(value.Color.ToArgb()));
+                text.Text = value?.ToString() ?? "";
+                var color = value?.Color ?? System.Drawing.Color.Red;
+                text.TextColor = UIColor.FromRGBA(color.R, color.G, color.G, color.A);
+                InvalidateIntrinsicContentSize();
             }
         }
     }
